a9a06117373513f816c02ce84af429f25bdd24fb
Anton Tymchyk <tony.zenekis@gmail.com>
2024-05-23 15:07:28 +0300

    added styles for card when timer run out and grid for cardList

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,22 @@
 import { useEffect, useState } from 'react'
 import styles from './Card.module.scss'
 
-const Card = (props) => {
-	const { id, title, comment, status, date } = props;
+const Card = props => {
+	const { id, title, comment, status, date } = props
 
-	// console.log('Timer >> ', new Date(date))
-	// // console.log(new Date)
+	const [timeAlert, setTimeAlert] = useState(false)
 
 	const calculateTimeLeft = () => {
-		const difference = +new Date(date) - +new Date();
-		let timeLeft = {};
+		const difference = +new Date(date) - +new Date()
+		let timeLeft = {}
 
 		if (difference > 0) {
 			timeLeft = {
 				days: Math.floor(difference / (1000 * 60 * 60 * 24)),
 				hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
 				minutes: Math.floor((difference / 1000 / 60) % 60),
-				seconds: Math.floor((difference / 1000) % 60)
-			};
+				seconds: Math.floor((difference / 1000) % 60),
+			}
 		} else {
 			timeLeft = {
 				days: '00',
@@ -27,30 +26,53 @@ const Card = (props) => {
 			}
 		}
 
-		return timeLeft;
+		return timeLeft
 	}
 
-	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+
+		useEffect(() => {
+			if (
+				timeLeft.days === '00' &&
+				timeLeft.hours === '00' &&
+				timeLeft.minutes === '00' &&
+				timeLeft.seconds === '00'
+			) {
+				setTimeAlert(true)
+			}
+		}, [timeLeft])
 
 	useEffect(() => {
-		const timer = setTimeout(() => {
-			setTimeLeft(calculateTimeLeft());
-		}, 1000);
+		const timer = setInterval(() => {
+			setTimeLeft(calculateTimeLeft())
+		}, 1000)
 
-		return () => clearTimeout(timer);
-	})
+		return () => clearInterval(timer)
+	}, [date])
 
-	let days = '';
+	let days = ''
 
 	if (timeLeft.days === 1) {
-		days = '1 Day';
+		days = '1 Day'
 	} else if (timeLeft.days > 1) {
-		days = `${timeLeft.days} Days`;
+		days = `${timeLeft.days} Days`
 	}
 
 	return (
-		<div className={styles.card} id={id}>
-			<div className={`${styles.timer} ${status === 'Hot' ? styles.hotStatus : status === 'Important' ? styles.importantStatus : ''}`}>{days} {timeLeft.hours.toString().padStart(2, '0')}:{timeLeft.minutes.toString().padStart(2, '0')}:{timeLeft.seconds.toString().padStart(2, '0')}</div>
+		<div className={`${styles.card} ${timeAlert === true ? styles.cardAlert : ''}`} id={id}>
+			<div
+				className={`${styles.timer} ${
+					status === 'Hot'
+						? styles.hotStatus
+						: status === 'Important'
+						? styles.importantStatus
+						: ''
+				} ${timeAlert === true ? styles.timeAlert : ''}`}
+			>
+				{days} {timeLeft.hours.toString().padStart(2, '0')}:
+				{timeLeft.minutes.toString().padStart(2, '0')}:
+				{timeLeft.seconds.toString().padStart(2, '0')}
+			</div>
 			<div className={styles.cardBlock}>
 				<p className={styles.ticker}>ToDo:</p>
 				<p className={styles.toDoText}>{title}</p>
