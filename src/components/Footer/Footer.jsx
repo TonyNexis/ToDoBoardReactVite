534246e56655984f243ebc5e609514fd0dbc0d6e
Anton <tony.zenekis@gmail.com>
2024-06-09 23:15:51 +0300

    some footer style changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
 import ButtonAddCardFooter from '../ButtonAddCardFooter/ButtonAddCardFooter'
+import Filter from '../Filter/Filter'
 import Clock from './../Clock/Clock'
-import Filter from '../filter/Filter'
 
 import styles from './Footer.module.scss'
 
 const Footer = () => {
 	return (
 		<div className={styles.footer}>
-			<ButtonAddCardFooter/>
-			<Clock />
-			<Filter/>
+			<ButtonAddCardFooter />
+			<div className={styles.container}>
+				<Filter />
+				<Clock />
+			</div>
 		</div>
 	)
 }
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,12 +1,24 @@
 import styles from './Filter.module.scss'
+import { Switch } from '@mui/material'
 
 const Filter = () => {
+    const label = { inputProps: { 'aria-label': 'Color switch demo' } };
+
     return (
         <div className={styles.filterWrapper}>
             <p>Filter:</p>
+            <div className={styles.switchWrapper}>
+            <Switch {...label} className={styles.switch} defaultChecked color="error"/>
             <p>Hot</p>
+            </div>
+            <div className={styles.switchWrapper}>
+            <Switch {...label} className={styles.switch} defaultChecked color="success"/>
             <p>Important</p>
+            </div>
+            <div className={styles.switchWrapper}>
+            <Switch {...label} className={styles.switch} defaultChecked color="primary"/>
             <p>Normal</p>
+            </div>
         </div>
     )
 }
