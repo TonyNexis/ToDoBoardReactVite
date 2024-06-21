d4f2037377f4e07bd2daaccd2ff465189e27e330
Anton Tymchyk <tony.zenekis@gmail.com>
2024-06-21 13:21:10 +0300

    work on menu

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,26 @@
-import { useState } from 'react'
 import { useLocation } from '@tanstack/react-router'
 import ButtonAddCardFooter from '../ButtonAddCardFooter/ButtonAddCardFooter'
 import Clock from './../Clock/Clock'
 import Filter from './../Filter/Filter'
-
 import styles from './Footer.module.scss'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleShowMenu } from '../../redux/showMenuSlice'
 
 const Footer = () => {
+	const dispatch = useDispatch();
 	const location = useLocation()
 
 	const isHomePage = location.pathname === '/'
 
-	const toggleNavBtn = useSelector((state) =>)
-	const [toggleNavBtn, setToggleNavBtn] = useState(false)
+	const toggleNavBtn = useSelector(state => state.showMenu.MenuIsOpen)
+
+	const onToggleNavBtn = () => {
+		dispatch(toggleShowMenu())
+	}
 
 	return (
 		<div className={styles.footer}>
-			<div className={`${styles.toggleNavBtn} ${toggleNavBtn ? styles.open : null}`} onClick={() => setToggleNavBtn(!toggleNavBtn)}>
+			<div className={`${styles.toggleNavBtn} ${toggleNavBtn ? styles.open : null}`} onClick={onToggleNavBtn}>
 				<span></span>
 				<span></span>
 				<span></span>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,41 @@
-import { Link }from '@tanstack/react-router'
+import { Link } from '@tanstack/react-router'
+import { useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleShowMenu } from '../../redux/showMenuSlice'
 import styles from './Menu.module.scss'
 
 const Menu = () => {
-    return (
-        <div className={styles.menu}>
-		<Link to='/'>Home</Link> 
-        <Link to='/about'>About</Link>
-	</div>
-    )
+	const showMenu = useSelector(state => state.showMenu.MenuIsOpen)
+	const menuRef = useRef(null)
+    const dispatch = useDispatch();
+
+	const handleClickOutside = (event) => {
+		if (menuRef.current && !menuRef.current.contains(event.target)) {
+			dispatch(toggleShowMenu())
+		}
+	}
+
+	useEffect(() => {
+		if (showMenu) {
+			document.addEventListener('mousedown', handleClickOutside)
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [showMenu])
+
+	return (
+		<div
+			ref={menuRef}
+			className={`${styles.menu} ${showMenu ? styles.active : null} `}
+		>
+			<Link to='/'>Home</Link>
+			<Link to='/about'>About</Link>
+		</div>
+	)
 }
 
 export default Menu
