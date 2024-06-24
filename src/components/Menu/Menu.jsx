4f2bf619444de2dc4c026e8f4e0427fe9bf41e63
Anton Tymchyk <tony.zenekis@gmail.com>
2024-06-24 22:45:02 +0300

    active page styles for menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { Link, useMatch } from '@tanstack/react-router'
+import { Link, useLocation } from '@tanstack/react-router'
 import { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleShowMenu } from '../../redux/showMenuSlice'
@@ -33,24 +33,33 @@ const Menu = ({ footerBtnToggleMenuRef }) => {
 		}
 	}, [showMenu])
 
-	const matches = {
-		home: useMatch({ from: '/' }),
-		// profile: useMatch({ from: '/profile', }),
-		// somePage: useMatch({ from: '/somePage'  }),
-		// about: useMatch({ from: '/about' }),
-	}
+	const location = useLocation()
+
+	const isActive = path => location.pathname === path
 
 	return (
 		<div
 			ref={menuRef}
 			className={`${styles.menu} ${showMenu ? styles.active : ''} `}
 		>
-			<Link className={matches.home ? styles.activeLink : ''} to='/'>
+			<Link className={isActive('/') ? styles.activeLink : ''} to='/'>
 				Home
 			</Link>
-			<Link className={matches.profile ? styles.activeLink : ''} to='/profile'>Profile</Link>
-			<Link className={matches.somePage ? styles.activeLink : ''} to='/somePage'>Some Page</Link>
-			<Link className={matches.about ? styles.activeLink : ''} to='/about'>About</Link>
+			<Link
+				className={isActive('/profile') ? styles.activeLink : ''}
+				to='/profile'
+			>
+				Profile
+			</Link>
+			<Link
+				className={isActive('/somepage') ? styles.activeLink : ''}
+				to='/somepage'
+			>
+				Some Page
+			</Link>
+			<Link className={isActive('/about') ? styles.activeLink : ''} to='/about'>
+				About
+			</Link>
 		</div>
 	)
 }
